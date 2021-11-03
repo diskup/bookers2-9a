cc6c85b0be8ab5910aab943abe9903d9432be6a1
“diskup” <taigaa.1998@gmail.com>
2021-11-03 09:34:26 +0000

    応用課題9a修正

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,86 @@
+class BooksController < ApplicationController
+  before_action :ensure_current_user, only: [:edit]
+
+  def ensure_current_user
+    @book = Book.find(params[:id])
+    redirect_to books_path if @book.user.id != current_user.id
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      impressionist(@book, nil, unique: [:ip_address])
+    end
+    @user = @book.user
+    @book_comment = BookComment.new
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      unless @isRoom
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+  end
+
+  def index
+    @book = Book.new
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort {|a,b|
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+      flash[:notice] = "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/about' => 'homes#about'
+  root 'homes#top'
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followers, on: :member
+    get :followeds, on: :member
+  end
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  get '/search', to: 'searches#search'
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create,:show]
+end
